b3af3a7b3be58ee7c6a91590f4e444f3819cdca0
Dan-Goodrick <dan@baywatchtech.com>
2020-04-08 21:57:53 -0600

    Using Lenovo Hostname

--- receive_client.py
+++ receive_client.py
@@ -3,7 +3,9 @@ import sys
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('192.168.0.10', 9000)
+HOST = socket.gethostname()
+print("Host: ", HOST)
+server_address = (HOST, 9000)
 sock.bind(server_address)
 print(f"Waiting to receive")
 while True:
